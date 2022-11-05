2400371e03268ea3ae0aec8f9270987ca8ea3e78
WaltVenditi <waltvenditti@gmail.com>
2022-11-05 10:27:31 -0400

    implementing mongoose models

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("dotenv").config();
+
+// process.env.<variable_name>
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+const mongoose = require("mongoose");
+const mongoDB = `mongodb+srv://${process.env.MDBusername}:${process.env.MDBpassword}@cluster0.ujaqqyn.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
